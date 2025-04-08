5ece72a6efd2b42d482d37bde543331ffdf8f7e3
matter <doesnt>
2025-04-08 21:09:04 +0300

    seq implementation attempt

--- SpMSpM/kernelCPU0.cu
+++ SpMSpM/kernelCPU0.cu
@@ -2,7 +2,14 @@
 #include "common.h"
 
 void spmspm_cpu0(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3) {
-
+	for(int row = 0 ; row < cooMatrix1.numRows ; ++row){
+		for(int col = 0 ; col < cooMatrix2.numCols ; ++col){
+			for(int i =0 ; i < cooMatrix1.numCols ; ++i){
+				// [row][col]
+				cooMatrix3.values[row * cooMatrix3.numCols + col] += cooMatrix1.values[row * cooMatrix1.numCols + i] * cooMatrix2.values[i * cooMatrix2.numCols + col];
+			}
+		}
+	}
 
 
 
