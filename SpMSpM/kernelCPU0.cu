3e6f2eb469acc2da3f73c57738dc554e1a3e150f
MohamadKhaledCharaf <khaledsharaf03@gmail.com>
2025-04-10 19:06:14 +0300

    COO CSR optimized with map<pair<int,int>,float> running time 50 ms

--- SpMSpM/kernelCPU0.cu
+++ SpMSpM/kernelCPU0.cu
@@ -1,7 +1,7 @@
 
 #include "common.h"
-
-
+#include <map>
+using namespace std ; 
 float add_row_col(unsigned int* rowPtrs, unsigned int* colPtrs, float* r_values, float* c_values, int num_row_elements, int num_col_elements){
 
 	float sum = 0;
@@ -22,27 +22,51 @@ float add_row_col(unsigned int* rowPtrs, unsigned int* colPtrs, float* r_values,
 
 
 
+// void spmspm_cpu0(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3) {
+
+// 	// matrix 1 as CSR
+// 	// matrix 2 as CSC
+
+// 	for(int i = 0; i < csrMatrix1->numRows; i++){
+// 		for(int j = 0; j < cscMatrix2->numCols; j++){
+// 			int num_elements_row = csrMatrix1->rowPtrs[i + 1] - csrMatrix1->rowPtrs[i];
+// 			int num_elements_col = cscMatrix2->colPtrs[j + 1] - cscMatrix2->colPtrs[j];
+// 			float addant = add_row_col(&csrMatrix1->colIdxs[csrMatrix1->rowPtrs[i]], &cscMatrix2->rowIdxs[cscMatrix2->colPtrs[j]],&csrMatrix1->values[csrMatrix1->rowPtrs[i]],&cscMatrix2->values[cscMatrix2->colPtrs[j]], num_elements_row, num_elements_col);
+
+// 			if(addant != 0){
+// 				int index = cooMatrix3->numNonzeros;
+// 				cooMatrix3->numNonzeros++;
+// 				cooMatrix3->rowIdxs[index] = i;
+// 				cooMatrix3->colIdxs[index] = j;
+// 				cooMatrix3->values[index] = addant;
+// 			}
+
+// 		}
+// 	}
+	
+// }
+
 void spmspm_cpu0(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3) {
 
-	// matrix 1 as CSR
-	// matrix 2 as CSC
-
-	for(int i = 0; i < csrMatrix1->numRows; i++){
-		for(int j = 0; j < cscMatrix2->numCols; j++){
-			int num_elements_row = csrMatrix1->rowPtrs[i + 1] - csrMatrix1->rowPtrs[i];
-			int num_elements_col = cscMatrix2->colPtrs[j + 1] - cscMatrix2->colPtrs[j];
-			float addant = add_row_col(&csrMatrix1->colIdxs[csrMatrix1->rowPtrs[i]], &cscMatrix2->rowIdxs[cscMatrix2->colPtrs[j]],&csrMatrix1->values[csrMatrix1->rowPtrs[i]],&cscMatrix2->values[cscMatrix2->colPtrs[j]], num_elements_row, num_elements_col);
-
-			if(addant != 0){
-				int index = cooMatrix3->numNonzeros;
-				cooMatrix3->numNonzeros++;
-				cooMatrix3->rowIdxs[index] = i;
-				cooMatrix3->colIdxs[index] = j;
-				cooMatrix3->values[index] = addant;
-			}
-
+// matrix 1 as COO cooMatrix1
+// matrix 2 as CSR csrMatrix2
+	map<pair<int,int>,float> result ; 
+	for(int i = 0; i < cooMatrix1->numNonzeros; ++i){
+		int rowNumber = cooMatrix1 -> rowIdxs[i];
+		int colNumber = cooMatrix1 -> colIdxs[i];
+		float val = cooMatrix1 -> values[i];
+		int csrStart = csrMatrix2 -> rowPtrs[colNumber];
+		int csrEnd = csrMatrix2 -> rowPtrs[colNumber+1];
+		for(int j = csrStart ; j < csrEnd ; ++j){
+			int colNumber2 = csrMatrix2 -> colIdxs[j];
+			float val2 = csrMatrix2 -> values[j];
+			result[{rowNumber,colNumber2}] += val*val2 ; 
 		}
 	}
-	
+	for(auto & p : result){
+		int index = cooMatrix3->numNonzeros++;
+		cooMatrix3->rowIdxs[index] = p.first.first ; 
+		cooMatrix3->colIdxs[index] = p.first.second ; 
+		cooMatrix3->values[index] = p.second ;
+	}
 }
-
