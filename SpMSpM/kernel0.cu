ae9eba2c1ce5a16aa373c35ca5c0ef43f6c1d4a3
Jad <jad-abou-hawili@hotmail.com>
2025-04-12 08:59:12 +0300

    parallel implementation, don't know how to do grid wide barrier synch with cooperative groups...

--- SpMSpM/kernel0.cu
+++ SpMSpM/kernel0.cu
@@ -1,16 +1,38 @@
 
 #include "common.h"
+#include <unordered_map>
+using namespace std;
+void spmspm_gpu0(COOMatrix *cooMatrix1, CSRMatrix *csrMatrix1,
+                 CSCMatrix *cscMatrix1, COOMatrix *cooMatrix2,
+                 CSRMatrix *csrMatrix2, CSCMatrix *cscMatrix2,
+                 COOMatrix *cooMatrix3, unsigned int numRows1,
+                 unsigned int numRows2, unsigned int numCols2,
+                 unsigned int numNonzeros1, unsigned int numNonzeros2) {
 
-void spmspm_gpu0(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3, unsigned int numRows1, unsigned int numRows2, unsigned int numCols2, unsigned int numNonzeros1, unsigned int numNonzeros2) {
+  // matrix 1 as COO cooMatrix1
+  // matrix 2 as CSR csrMatrix2
+  unordered_map<int, float> result;
+  int numColumnsOutputMatrix = cooMatrix3->numCols;
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  if (i < cooMatrix1->numNonzeros) {
+    int rowNumber = cooMatrix1->rowIdxs[i];
+    int colNumber = cooMatrix1->colIdxs[i];
+    float val = cooMatrix1->values[i];
+    int csrStart = csrMatrix2->rowPtrs[colNumber];
+    int csrEnd = csrMatrix2->rowPtrs[colNumber + 1];
+    for (int j = csrStart; j < csrEnd; ++j) {
+      int colNumber2 = csrMatrix2->colIdxs[j];
+      float val2 = csrMatrix2->values[j];
+      int oneToOneIndex = rowNumber * numColumnsOutputMatrix + colNumber2;
+      result[oneToOneIndex] += val * val2;
+    }
+  }
 
-
-
-
-
-
-
-
-
-
+  // synchronize across all blocks
+  for (auto &p : result) {
+    int index = cooMatrix3->numNonzeros++;
+    cooMatrix3->rowIdxs[index] = p.first / numColumnsOutputMatrix;
+    cooMatrix3->colIdxs[index] = p.first % numColumnsOutputMatrix;
+    cooMatrix3->values[index] = p.second;
+  }
 }
-
--- SpMSpM/kernelCPU0.cu
+++ SpMSpM/kernelCPU0.cu
@@ -1,74 +1,34 @@
 
 #include "common.h"
 #include <unordered_map>
-using namespace std ; 
-float add_row_col(unsigned int* rowPtrs, unsigned int* colPtrs, float* r_values, float* c_values, int num_row_elements, int num_col_elements){
+using namespace std;
 
-	float sum = 0;
+void spmspm_cpu0(COOMatrix *cooMatrix1, CSRMatrix *csrMatrix1,
+                 CSCMatrix *cscMatrix1, COOMatrix *cooMatrix2,
+                 CSRMatrix *csrMatrix2, CSCMatrix *cscMatrix2,
+                 COOMatrix *cooMatrix3) {
 
-	for(int i = 0; i < num_row_elements; i++){
-		for(int j = 0; j < num_col_elements; j++){
-			int index_row = rowPtrs[i];
-			int index_col = colPtrs[j];
-
-			if(index_row == index_col){
-				sum += r_values[i]*c_values[j];
-			}
-		}
-	}
-
-	return sum;
-}
-
-
-
-// void spmspm_cpu0(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3) {
-
-// 	// matrix 1 as CSR
-// 	// matrix 2 as CSC
-
-// 	for(int i = 0; i < csrMatrix1->numRows; i++){
-// 		for(int j = 0; j < cscMatrix2->numCols; j++){
-// 			int num_elements_row = csrMatrix1->rowPtrs[i + 1] - csrMatrix1->rowPtrs[i];
-// 			int num_elements_col = cscMatrix2->colPtrs[j + 1] - cscMatrix2->colPtrs[j];
-// 			float addant = add_row_col(&csrMatrix1->colIdxs[csrMatrix1->rowPtrs[i]], &cscMatrix2->rowIdxs[cscMatrix2->colPtrs[j]],&csrMatrix1->values[csrMatrix1->rowPtrs[i]],&cscMatrix2->values[cscMatrix2->colPtrs[j]], num_elements_row, num_elements_col);
-
-// 			if(addant != 0){
-// 				int index = cooMatrix3->numNonzeros;
-// 				cooMatrix3->numNonzeros++;
-// 				cooMatrix3->rowIdxs[index] = i;
-// 				cooMatrix3->colIdxs[index] = j;
-// 				cooMatrix3->values[index] = addant;
-// 			}
-
-// 		}
-// 	}
-	
-// }
-
-void spmspm_cpu0(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3) {
-
-// matrix 1 as COO cooMatrix1
-// matrix 2 as CSR csrMatrix2
-	unordered_map<int,float> result ;  
-	int numColumnsOutputMatrix = cooMatrix3 -> numCols; 
-	for(int i = 0; i < cooMatrix1->numNonzeros; ++i){
-		int rowNumber = cooMatrix1 -> rowIdxs[i];
-		int colNumber = cooMatrix1 -> colIdxs[i];
-		float val = cooMatrix1 -> values[i];
-		int csrStart = csrMatrix2 -> rowPtrs[colNumber];
-		int csrEnd = csrMatrix2 -> rowPtrs[colNumber+1];
-		for(int j = csrStart ; j < csrEnd ; ++j){
-			int colNumber2 = csrMatrix2 -> colIdxs[j];
-			float val2 = csrMatrix2 -> values[j];
-			int oneToOneIndex = rowNumber*numColumnsOutputMatrix + colNumber2 ; 
-			result[oneToOneIndex] += val*val2 ; 
-		}
-	}
-	for(auto & p : result){
-		int index = cooMatrix3->numNonzeros++;
-		cooMatrix3->rowIdxs[index] = p.first / numColumnsOutputMatrix ; 
-		cooMatrix3->colIdxs[index] = p.first % numColumnsOutputMatrix ; 
-		cooMatrix3->values[index] = p.second;
-	}
+  // matrix 1 as COO cooMatrix1
+  // matrix 2 as CSR csrMatrix2
+  unordered_map<int, float> result;
+  int numColumnsOutputMatrix = cooMatrix3->numCols;
+  for (int i = 0; i < cooMatrix1->numNonzeros; ++i) {
+    int rowNumber = cooMatrix1->rowIdxs[i];
+    int colNumber = cooMatrix1->colIdxs[i];
+    float val = cooMatrix1->values[i];
+    int csrStart = csrMatrix2->rowPtrs[colNumber];
+    int csrEnd = csrMatrix2->rowPtrs[colNumber + 1];
+    for (int j = csrStart; j < csrEnd; ++j) {
+      int colNumber2 = csrMatrix2->colIdxs[j];
+      float val2 = csrMatrix2->values[j];
+      int oneToOneIndex = rowNumber * numColumnsOutputMatrix + colNumber2;
+      result[oneToOneIndex] += val * val2;
+    }
+  }
+  for (auto &p : result) {
+    int index = cooMatrix3->numNonzeros++;
+    cooMatrix3->rowIdxs[index] = p.first / numColumnsOutputMatrix;
+    cooMatrix3->colIdxs[index] = p.first % numColumnsOutputMatrix;
+    cooMatrix3->values[index] = p.second;
+  }
 }
