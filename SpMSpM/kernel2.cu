5904477d247bde73beefc88dd7bb8ec1fd54afb3
Mohamad Ayman Charaf <mcharaf@lxplus938.cern.ch>
2025-05-02 00:55:06 +0200

    Apparently we can't do our approach of filtering the 2nd matrix after flagging the enteries we visited at every iteration of the loop, because this will lead to race conditions :/, I applied a fix now it's lets efficient but works

--- SpMSpM/kernel2.cu
+++ SpMSpM/kernel2.cu
@@ -4,58 +4,59 @@
 #define COL_OUTPUT_SIZE 512
 #define EMPTY_CELL std::numeric_limits<float>::max()
 
-__global__ mul_kernel_opt_2(CSRMatrix* csrMatrix1,CSRMatrix* csrMatrix2,COOMatrix* cooMatrix3,unsigned int numColM2){
 
-    int numLoops = (numColM2 + COL_OUTPUT_SIZE - 1)/COL_OUTPUT_SIZE;
+__global__ void mul_kernel_opt_2(CSRMatrix* csrMatrix1, CSRMatrix* csrMatrix2, COOMatrix* cooMatrix3, unsigned int numColM2) {
+    int numLoops = (numColM2 + COL_OUTPUT_SIZE - 1) / COL_OUTPUT_SIZE;
 
-    __shared__  float rowOutput_s[COL_OUTPUT_SIZE];
+    __shared__ float rowOutput_s[COL_OUTPUT_SIZE];
 
-    for(int counterLoop = 1; counterLoop <= numLoops; ++counterLoop){
+    int rowIdxM1 = blockIdx.x;
+    int rowStartM1 = csrMatrix1->rowPtrs[rowIdxM1];
+    int rowEndM1 = csrMatrix1->rowPtrs[rowIdxM1 + 1];
 
-        for(int i = threadIdx.x; i < COL_OUTPUT_SIZE; i += blockDim.x){
-            rowOutput_s[i] = 0;
+    for (int counterLoop = 0; counterLoop < numLoops; ++counterLoop) {
+        int start_col = counterLoop * COL_OUTPUT_SIZE;
+        int end_col = min(start_col + COL_OUTPUT_SIZE, numColM2);
+
+        // Initialize shared memory
+        for (int i = threadIdx.x; i < COL_OUTPUT_SIZE; i += blockDim.x) {
+            rowOutput_s[i] = 0.0f;
         }
+        __syncthreads();
 
-        int rowIdxM1 = blockIdx.x ; 
-        int rowSizeM1 = csrMatrix1 -> rowPtrs[rowIdxM1+1] - csrMatrix1 -> rowPtrs[rowIdxM1];
-        int startingIndexInRowM1 = csrMatrix1 -> rowPtrs[rowIdxM1] + threadIdx.x ; // Assign a thread to each entry in row
-        for(int varStartingRowIndexM1 = startingIndexInRowM1 ; varStartingRowIndexM1 < rowSizeM1 ; varStartingRowIndexM1 += blockDim.x ){
-            int colIndexM1 = csrMatrix1 -> colIdxs[varStartingRowIndexM1] ;
-            float valM1 = csrMatrix1 -> values[varStartingRowIndexM1] ;
-            int rowSizeM2 = csrMatrix2 -> rowPtrs[colIndexM1+1] - csrMatrix2 -> rowPtrs[colIndexM1];
-            int startingIndexInRowM2 = csrMatrix2 -> rowPtrs[colIndexM1] ;
-            int emptyCellIdx = -1 ; 
-            int numEmptyElements = 0 ; 
-            for(int varStartingRowIndexM2 = startingIndexInRowM2 ; varStartingRowIndexM2 < rowSizeM2 ; ++varStartingRowIndexM2){
-                float valM2 = csrMatrix2 -> values[varStartingRowIndexM2] ;
-                int colIndexM2 = csrMatrix2 -> colIdxs[varStartingRowIndexM2] ;
-                if(colIndexM2 < COL_OUTPUT_SIZE*counterLoop){
-                    rowOutput_s[colIndexM2] += valM1 * valM2 ;
-                    if(numEmptyElements == 0){
-                        emptyCellIdx = varStartingRowIndexM2 ;
-                    }
-                    numEmptyElements +=1 ; 
-                }
-                else{
-                    if(emptyCellIdx != -1){
-                        csrMatrix2 -> values[emptyCellIdx] = csrMatrix2 -> values[varStartingRowIndexM2] ;
-                        csrMatrix2 -> colIdxs[emptyCellIdx] = csrMatrix2 -> values[varStartingRowIndexM2] ;
-                        numEmptyElements -= 1 ;
-                        emptyCellIdx +=1 ; 
-                    }                
+        // Process each element in csrMatrix1's current row
+        for (int elemIdxM1 = rowStartM1 + threadIdx.x; elemIdxM1 < rowEndM1; elemIdxM1 += blockDim.x) {
+            int colIndexM1 = csrMatrix1->colIdxs[elemIdxM1];
+            float valM1 = csrMatrix1->values[elemIdxM1];
+
+            int rowStartM2 = csrMatrix2->rowPtrs[colIndexM1];
+            int rowEndM2 = csrMatrix2->rowPtrs[colIndexM1 + 1];
+
+            for (int elemIdxM2 = rowStartM2; elemIdxM2 < rowEndM2; ++elemIdxM2) {
+                int colIndexM2 = csrMatrix2->colIdxs[elemIdxM2];
+                float valM2 = csrMatrix2->values[elemIdxM2];
+
+                if (colIndexM2 >= start_col && colIndexM2 < end_col) {
+                    int local_col = colIndexM2 - start_col;
+                    atomicAdd(&rowOutput_s[local_col], valM1 * valM2);
                 }
             }
-            __syncthreads();
-            for(int outputIdx = threadIdx.x ; outputIdx < COL_OUTPUT_SIZE ; outputIdx+= blockDim.x){
-                if(rowOutput_s[outputIdx] != 0){
-                    int index = atomicAdd(&cooMatrix3->numNonzeros,1);
+        }
+        __syncthreads();
+
+        // Write non-zero values to COO
+        for (int i = threadIdx.x; i < COL_OUTPUT_SIZE; i += blockDim.x) {
+		if (fabsf(rowOutput_s[i]) > 1e-7f)  {
+                int global_col = start_col + i;
+                if (global_col < numColM2) { // Ensure within matrix bounds
+                    int index = atomicAdd(&cooMatrix3->numNonzeros, 1);
                     cooMatrix3->rowIdxs[index] = rowIdxM1;
-                    cooMatrix3->colIdxs[index] = 1;// to be computed;
-                    cooMatrix3->values[index] = rowOutput_s[outputIdx];
-
+                    cooMatrix3->colIdxs[index] = global_col;
+                    cooMatrix3->values[index] = rowOutput_s[i];
                 }
             }
         }
+        __syncthreads();
     }
 }
 
@@ -65,6 +66,8 @@ __global__ mul_kernel_opt_2(CSRMatrix* csrMatrix1,CSRMatrix* csrMatrix2,COOMatri
 void spmspm_gpu2(COOMatrix* cooMatrix1, CSRMatrix* csrMatrix1, CSCMatrix* cscMatrix1, COOMatrix* cooMatrix2, CSRMatrix* csrMatrix2, CSCMatrix* cscMatrix2, COOMatrix* cooMatrix3, unsigned int numRows1, unsigned int numRows2, unsigned int numCols2, unsigned int numNonzeros1, unsigned int numNonzeros2) {
 
     int numThreadsPerBlock = 512;
+    cudaMemset(&cooMatrix3->numNonzeros, 0, sizeof(int));
+
 
     // num rows of first matrix
     int numBlocks = numRows1; 
